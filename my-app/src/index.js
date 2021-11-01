cac4997966ecaca35e92c8dd4d7c580a8294b06b
Ole Markus Kristiansen Ternø <olmt@equinor.com>
2021-11-01 13:42:33 +0100

    remove paragraph

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,7 +7,6 @@ ReactDOM.render(
     <h1>Hello, EDC!</h1>
     <p>Secret value: {process.env.REACT_APP_SECRET}</p>
     <p>Environment variable value: {process.env.REACT_APP_VARIABLE}</p>
-    <p>Radix is awesome.</p>
   </>,
   document.getElementById('root')
 );
